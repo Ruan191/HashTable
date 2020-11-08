533cb99388c734b4d6fdd73fb6636ff20c8cc96e
Ruan191 <64075904+Ruan191@users.noreply.github.com>
2020-11-08 23:40:34 +0200

    A hash table for strings
    A hash table made for strings using Java the Hash.java is the example of while the HashTable is where the magic happens. You can use this hash table to check if a string exist in the database super fast

---
+++ hash/src/hash/Hash.java
@@ -0,0 +1,26 @@
+
+package hash;
+import java.util.Scanner;
+
+public class Hash {
+
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable();
+        
+        String[] Names = {"a","Bob","Robert","Sam","Zander","Ruan","Pietter","John","Loki","Samual","Xander","Aatrox","Yumi"};
+        
+        System.out.println("Loading into memory...");       
+        for (String Name : Names) {
+            hashTable.hashMap(Name, hashTable.hashKey(Name));
+        }
+        System.out.println("Done.");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter text to be searched");
+        String userInput = input.nextLine();
+        
+        System.out.println(hashTable.hashStringValueExist(userInput));
+
+    }    
+    
+}
---
+++ hash/src/hash/HashTable.java
@@ -0,0 +1,85 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2020 MooMoo191.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+/**
+ *
+ * @author MooMoo191
+ */
+package hash;
+
+public class HashTable {
+       
+    private String[][] hashArray;
+    
+    public HashTable(){
+        hashArray = new String[1000][100];
+    }
+    
+    public HashTable(int hashArraySize, int hashStackArraySize){
+        hashArray = new String[hashArraySize][hashStackArraySize];
+    }
+
+    public int hashKey(String text){
+        int value = 0;
+        
+        for (int i = 0; i < text.length(); i++){
+            value += ((int)text.toLowerCase().toCharArray()[i] - 97);
+        }
+        
+        return value;
+    }
+    
+    //loads the Strings into the indexes and stacks (if needed)
+    public void hashMap(String text, int index){
+        int indexAppend = 0;
+
+        if (hashArray[index] != null){
+            while(hashArray[index][indexAppend] != null){
+                indexAppend++;
+            }
+        }
+
+        hashArray[index][indexAppend] = text;
+        System.out.println(text + " at index " + (index) + " on stack " + indexAppend);
+    }
+    
+    //Looks for the String in the index and stacks
+    public boolean hashStringValueExist(String text){
+        int indexAppend = 0;
+        
+        if (!text.equals(hashArray[hashKey(text)][0])){
+            while(hashArray[hashKey(text)][indexAppend] != null && indexAppend != 101){
+                if (hashArray[hashKey(text)][indexAppend].equals(text)){
+                    return true;                  
+                }else{
+                    indexAppend++;
+                }
+              
+            }
+        }else{
+            return true;
+        }
+        return false;
+    }
+}
